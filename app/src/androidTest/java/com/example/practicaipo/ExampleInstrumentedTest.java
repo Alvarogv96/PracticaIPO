9dd22902cb2f32f042b161f30b6a82b7580f4e42
adrian <adriansr2010@gmail.com>
2020-03-06 12:11:37 +0100

    Revert "Comento una gilip"
    This reverts commit 373ea087756d4b3fd708eac20d8453fd500a2613.

--- app/src/androidTest/java/com/example/practicaipo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/practicaipo/ExampleInstrumentedTest.java
@@ -23,6 +23,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.practicaipo", appContext.getPackageName());
-		//holi
     }
 }
